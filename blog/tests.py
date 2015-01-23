53650f92adc4447cc36c2289525d6047834b910f
emilabraham <emilabraham1414@gmail.com>
2015-01-22 23:09:10 -0500

    Added a few tests and added future post capability.

--- blog/tests.py
+++ blog/tests.py
@@ -11,9 +11,9 @@ def create_post(title, author, slug, body):
   return Post.objects.create(title=title,pub_date=timezone.now(),author=author,
       slug=slug,body=body)
 
-class PostIndexViewTests(TestCase):
-  def test_index_view_with_a_post(self):
-    """ 
+  class PostIndexViewTests(TestCase):
+    def test_index_view_with_a_post(self):
+      """ 
     The index view should create a link with the title as a link.
     """
     new_post = create_post(title="New Post",author="Emil Abraham",
@@ -22,5 +22,23 @@ class PostIndexViewTests(TestCase):
     self.assertQuerysetEqual(
         response.context['all_posts'],
         ['<Post: New Post>']
-    )   
+        )   
 
+    def test_index_view_with_future_post(self):
+      """
+      The index view should be empty and display: No posts available
+      """
+    future_post = create_post(title="Future Post", author="Emil Abraham",
+        slug="future-post",body="This is the future body",
+        pub_date=timezone.now()+20)
+    response = self.client.get(reverse('blog:index'))
+    self.assertEqual(response.status_code, 200)
+    self.assertContains(response, "No posts are available")
+
+    def test_index_view_without_posts(self):
+      """
+      The index view should return No posts available
+      """
+      response = self.client.get(reverse('blog:index'))
+      self.assertEqual(reponse.status_code, 200)
+      self.assertContains(response, "No posts are available")
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+from django.utils import timezone
 
 def index(request):
   all_posts = Post.objects.order_by('-pub_date')
+  #Because it wouldn't all fit on one line.
+  all_posts = all_posts.filter(pub_date__lte=timezone.now())
   context = {'all_posts': all_posts}
   return render(request, 'blog/index.html', context)
 
